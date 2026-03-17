18cca9826dc614c429389c61ea45cd0a9b0e5e24
oxface <r.g.klapchuk@gmail.com>
2026-03-17 23:48:55 +0000

    chore: missing lint settings

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,12 @@
 export default {
-  '*.{ts,tsx,js,jsx}': ['prettier --write', 'eslint --fix'],
+  '*.{ts,tsx,js,jsx}': (filenames) => {
+    const cmds = [`prettier --write ${filenames.join(' ')}`];
+    const portalFiles = filenames.filter((f) => f.includes('web/'));
+    if (portalFiles.length > 0) {
+      cmds.push(`pnpm --filter @alcopilot/portal exec eslint --fix ${portalFiles.join(' ')}`);
+    }
+    return cmds;
+  },
   '*.{json,css,md,yaml,yml,html}': ['prettier --write'],
   '*.cs': (filenames) => {
     const files = filenames.map((f) => `--include ${f}`).join(' ');
